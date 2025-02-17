f8368aeaaf7051b6e7e3376e3d759a29194e41ce
farahalqedra <farah.abd1234@gmail.com>
2025-02-17 02:08:33 -0800

    Create NAME.java

---
+++ Project1/NAME.java
@@ -0,0 +1,10 @@
+public class Names {
+    
+    public static void main(String[] args) {
+        printFarahAlQedra(); // Corrected method call
+    }
+
+    public static void printFarahAlQedra() {
+        System.out.println("Farah Al Qedra");
+    }
+}
