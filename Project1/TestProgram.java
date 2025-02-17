9c64414467f9b8d6a30e7e32044607ea2a08d024
issamma1 <159276240+issamma1@users.noreply.github.com>
2025-02-17 15:39:57 +0400

    Update TestProgram.java
    Fixed Farah's

--- Project1/TestProgram.java
+++ Project1/TestProgram.java
@@ -1,10 +1,49 @@
 public class TestProgram {
-    
-    public static void main(String[] args) {
-        printFarahAlQedra(); // Corrected method call
-    }
 
-    public static void printFarahAlQedra() {
+	public static void main(String[] args) {
+		System.out.println("hello hello");
+	}
+	
+	public static int getYear() {
+		return 2025;
+	}
+	
+	public static int getDay() {
+		int x = 90;
+		return 5;
+	}
+	
+	public static void printIssamName() {
+		System.out.println("Issam");
+	}
+	
+	public static void printFurahName() {
+		System.out.println("Furah");
+	}
+	
+	public static void prinrSaifName() {
+		System.out.println("Saif");
+	}
+
+	public static void printAdelName() {
+		System.out.println("Adel");
+	}
+
+	public static void printTaymourName()
+	{
+		System.out.println("Taymour");
+	}
+
+	public static void printMohammedAliName()
+	{
+		System.out.println("Mohammed Ali");
+	} 
+	public static void printAliaName() {
+		System.out.println("Alia");
+	}
+	
+	public static void printFarahAlQedra() {
         System.out.println("Farah Al Qedra");
     }
+
 }
